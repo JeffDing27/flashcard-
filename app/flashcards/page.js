d29f5e51d6326f824859fa0884d8e569a2340fed
0xPorkchops <proulxcameron@icloud.com>
2024-08-22 01:14:05 -0400

    Added sign in requirement to view and create cards

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { db } from "@/firebase";
-import { useUser } from "@clerk/nextjs";
+import { useUser, SignedIn, SignedOut } from "@clerk/nextjs";
 import {
   Container,
   Box,
@@ -59,88 +59,103 @@ export default function ViewFlashcards() {
 
   return (
     <Container maxWidth="md">
-      <Box
-        sx={{
-          mt: 4,
-          mb: 6,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4">View Flashcards</Typography>
-        {Object.keys(flashcardsByDeck).length > 0 ? (
-          Object.entries(flashcardsByDeck).map(([deckName, flashcards]) => (
-            <Accordion key={deckName} sx={{ width: "100%", mt: 2 }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls={`${deckName}-content`}
-                id={`${deckName}-header`}
-              >
-                <Typography variant="h6">{deckName}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Grid container spacing={3}>
-                  {flashcards.map((flashcard, index) => (
-                    <Grid item xs={12} md={4} key={index}>
-                      <Card>
-                        <CardContent
-                          onClick={() => handleCardClick(deckName, index)}
-                          sx={{
-                            perspective: "1000px",
-                            "& > div": {
-                              transition: "transform 0.6s",
-                              transformStyle: "preserve-3d",
-                              position: "relative",
-                              width: "100%",
-                              height: "200px",
-                              boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-                              transform: flipped[`${deckName}-${index}`]
-                                ? "rotateY(180deg)"
-                                : "rotateY(0deg)",
-                            },
-                            "& > div > div": {
-                              position: "absolute",
-                              width: "100%",
-                              height: "100%",
-                              backfaceVisibility: "hidden",
-                              display: "flex",
-                              justifyContent: "center",
-                              alignItems: "center",
-                              padding: 2,
-                              boxSizing: "border-box",
-                            },
-                            "& > div > div:nth-of-type(2)": {
-                              transform: "rotateY(180deg)",
-                            },
-                          }}
-                        >
-                          <div>
+      <SignedIn>
+        <Box
+          sx={{
+            mt: 4,
+            mb: 6,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h4">View Flashcards</Typography>
+          {Object.keys(flashcardsByDeck).length > 0 ? (
+            Object.entries(flashcardsByDeck).map(([deckName, flashcards]) => (
+              <Accordion key={deckName} sx={{ width: "100%", mt: 2 }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={`${deckName}-content`}
+                  id={`${deckName}-header`}
+                >
+                  <Typography variant="h6">{deckName}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Grid container spacing={3}>
+                    {flashcards.map((flashcard, index) => (
+                      <Grid item xs={12} md={4} key={index}>
+                        <Card>
+                          <CardContent
+                            onClick={() => handleCardClick(deckName, index)}
+                            sx={{
+                              perspective: "1000px",
+                              "& > div": {
+                                transition: "transform 0.6s",
+                                transformStyle: "preserve-3d",
+                                position: "relative",
+                                width: "100%",
+                                height: "200px",
+                                boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+                                transform: flipped[`${deckName}-${index}`]
+                                  ? "rotateY(180deg)"
+                                  : "rotateY(0deg)",
+                              },
+                              "& > div > div": {
+                                position: "absolute",
+                                width: "100%",
+                                height: "100%",
+                                backfaceVisibility: "hidden",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                padding: 2,
+                                boxSizing: "border-box",
+                              },
+                              "& > div > div:nth-of-type(2)": {
+                                transform: "rotateY(180deg)",
+                              },
+                            }}
+                          >
                             <div>
-                              <Typography variant="h5" component="div">
-                                {flashcard.front}
-                              </Typography>
+                              <div>
+                                <Typography variant="h5" component="div">
+                                  {flashcard.front}
+                                </Typography>
+                              </div>
+                              <div>
+                                <Typography variant="h5" component="div">
+                                  {flashcard.back}
+                                </Typography>
+                              </div>
                             </div>
-                            <div>
-                              <Typography variant="h5" component="div">
-                                {flashcard.back}
-                              </Typography>
-                            </div>
-                          </div>
-                        </CardContent>
-                      </Card>
-                    </Grid>
-                  ))}
-                </Grid>
-              </AccordionDetails>
-            </Accordion>
-          ))
-        ) : (
-          <Typography variant="h6" sx={{ mt: 4 }}>
-            No flashcards found.
-          </Typography>
-        )}
-      </Box>
+                          </CardContent>
+                        </Card>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </AccordionDetails>
+              </Accordion>
+            ))
+          ) : (
+            <Typography variant="h6" sx={{ mt: 4 }}>
+              No flashcards found.
+            </Typography>
+          )}
+        </Box>
+      </SignedIn>
+      <SignedOut>
+        <Box
+          sx={{
+            mt: 4,
+            mb: 6,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h4">Please log in to view flashcards</Typography>
+        </Box>
+      </SignedOut>
     </Container>
   );
 }
